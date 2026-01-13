4f51e8b60d1ae341fc2458716bf2b74035b15554
clement lion <clement.lionceau@gmail.com>
2026-01-13 14:48:13 +0100

    added JsonData check before trying to open a filepath

--- Game/src/Scenes/Menus/MenuLoader.cs
+++ Game/src/Scenes/Menus/MenuLoader.cs
@@ -7,8 +7,18 @@ using MenuData;
 
 public static class MenuDataLoader
 {
+    /// <summary>
+    /// Tries to deserialize a Json with information about a Menu.
+    /// </summary>
+    /// <param name="dataDirectory"></param>
+    /// <returns>A Deserialized Json data if the Deserialization operation succeeded. Else, it returns null.</returns>
+    /// <exception cref="FileNotFoundException"> Throws an error if the firectory specified does not exist</exception>
     public static JsonData? LoadMenuData(string dataDirectory)
     {
+        if (!Directory.Exists(dataDirectory))
+        {
+            throw new FileNotFoundException($"Could not find the following file: {dataDirectory}");
+        }
         string jsonString = File.ReadAllText(dataDirectory);
         var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
         JsonData? data = JsonSerializer.Deserialize<JsonData>(jsonString, options);
